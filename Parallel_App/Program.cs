121605d12edc28a949713bf04941912643238a63
reza farazi <rezafarazi1378@gmail.com>
2022-01-03 19:43:32 +0330

    OK

---
+++ Parallel_App/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parallel_App
+{
+    class Program
+    {
+
+        //Global variables
+        static int i = 1;
+
+        static void Main(string[] args)
+        {
+
+            Parallel.For(0, 2, get_work); 
+
+            Parallel.For(0, 2, delegate(int a) {
+                i = i * 10;
+            });
+
+            Parallel.For(0, 2, a => {
+                i = i * 10;
+            });
+
+            Console.WriteLine(i);
+            Console.ReadKey();
+
+        }
+
+        //Get a function
+        public static void get_work(int a)
+        {
+            i = i * 10;
+        }
+
+    }
+}
